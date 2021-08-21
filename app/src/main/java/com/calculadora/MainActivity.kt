2e9a542e2509759f3b29e9ecc554cf43a2e40270
Caio Vinicius <caioviniciuscv18@gmail.com>
2021-08-21 03:06:40 -0300

    fixed and refactored

--- app/src/main/java/com/calculadora/MainActivity.kt
+++ app/src/main/java/com/calculadora/MainActivity.kt
@@ -1,49 +1,77 @@
 package com.calculadora
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var soma: Button
+    private lateinit var subtracao: Button
+    private lateinit var multiplicacao: Button
+    private lateinit var divisao: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val operando1: EditText = findViewById(R.id.edtOperando1)
-        val operando2: EditText = findViewById(R.id.edtOperando2)
-        val soma: Button = findViewById(R.id.btnSoma)
-        val subtracao: Button = findViewById(R.id.btnSubtracao)
-        val multiplicacao: Button = findViewById(R.id.btnMultiplicacao)
-        val divisao: Button = findViewById(R.id.btnDivisao)
+        soma = findViewById(R.id.btnSoma)
+        soma.setOnClickListener(this)
 
-        soma.setOnClickListener{
-            val valor1: Float = operando1.text.toString().toFloat()
-            val valor2: Float = operando2.text.toString().toFloat()
-            val resultado: Float = valor1 + valor2
-            Toast.makeText(this,resultado.toString(), Toast.LENGTH_LONG).show()
+        subtracao = findViewById(R.id.btnSubtracao)
+        subtracao.setOnClickListener(this)
+
+        multiplicacao = findViewById(R.id.btnMultiplicacao)
+        multiplicacao.setOnClickListener(this)
+
+        divisao = findViewById(R.id.btnDivisao)
+        divisao.setOnClickListener(this)
+    }
+
+    private fun calculo(numero1: Double, numero2: Double, operador:String){
+
+        val txtResultado = findViewById<TextView>(R.id.txtResultado)
+        val resultado: Double
+
+        if(operador == "+"){
+            resultado = numero1 + numero2
+            txtResultado.text = resultado.toString()
+        }else if( operador == "-"){
+            resultado = numero1 - numero2
+            txtResultado.text = resultado.toString()
+        }else if (operador == "*"){
+            resultado = numero1 * numero2
+            txtResultado.text = resultado.toString()
+        }else{
+            resultado = numero1 / numero2
+            txtResultado.text = resultado.toString()
         }
 
-        subtracao.setOnClickListener{
-            val valor1: Float = operando1.text.toString().toFloat()
-            val valor2: Float = operando2.text.toString().toFloat()
-            val resultado: Float = valor1 - valor2
-            Toast.makeText(this,resultado.toString(), Toast.LENGTH_LONG).show()
+    }
+
+    override fun onClick(p0: View?) {
+        val numero1 = findViewById<EditText>(R.id.edtOperando1)
+        val numero2 = findViewById<EditText>(R.id.edtOperando2)
+        when(p0?.id){
+            R.id.btnSoma->{
+                calculo(numero1.text.toString().toDouble(), numero2.text.toString().toDouble(),"+")
+            }
+            R.id.btnSubtracao->{
+                calculo(numero1.text.toString().toDouble(), numero2.text.toString().toDouble(),"-")
+            }
+            R.id.btnMultiplicacao->{
+                calculo(numero1.text.toString().toDouble(), numero2.text.toString().toDouble(),"*")
+            }
+            R.id.btnDivisao->{
+                calculo(numero1.text.toString().toDouble(), numero2.text.toString().toDouble(),"/")
+            }
         }
 
-        multiplicacao.setOnClickListener{
-            val valor1: Float = operando1.text.toString().toFloat()
-            val valor2: Float = operando2.text.toString().toFloat()
-            val resultado: Float = valor1 * valor2
-            Toast.makeText(this,resultado.toString(), Toast.LENGTH_LONG).show()
-        }
 
-        divisao.setOnClickListener{
-            val valor1: Float = operando1.text.toString().toFloat()
-            val valor2: Float = operando2.text.toString().toFloat()
-            val resultado: Float = valor1 / valor2
-            Toast.makeText(this,resultado.toString(), Toast.LENGTH_LONG).show()
-        }
     }
 }
